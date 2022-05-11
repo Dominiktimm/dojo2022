f9434652dd8c79c6e4e8c1a874dda4f2a9946f34
Dominiktimm <84400910+Dominiktimm@users.noreply.github.com>
2022-05-11 16:39:58 +0200

    Add files via upload
    2.0

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
 "use strict";
 
 let grid_game = [
-    ["1", "1", "1"],
-    ["1", "1", "1"],
-    ["1", "1", "1"]
+    ["1","1","1","1","1"],
+    ["1","1","1","1","1"],
+    ["1","1","1","1","1"],
+    ["1","1","1","1","1"],
+    ["1","1","1","1","1"]
 ];
 
 let grid = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
+    [1,2,3,4,5],
+    [6,7,8,9,10],
+    [11,12,13,14,15],
+    [16,17,18,19,20],
+    [21,22,23,24,25]
+
 ];
 
 let game_over = false;
-// User Input - which light do you want to toggle
-    // Input into coordinates
-    // Check for neighbors 
-    // Switch input and neighbors status
 let game = {
     selection:[],
 
     input() {
         this.selection = [];
         let input_field = prompt("Please select field (1 - 9)");
-        if(input_field <= 9 && input_field > 0) {
+        if(input_field <= 25 && input_field > 0) {
             this.input_to_coordinates(input_field);
         } else {
-            if (confirm("To continue press OK")) {
+            if (confirm("Out of Gameboard - To continue press OK")) {
                 game.start();
               } else {
                 game_over = true;
@@ -36,9 +37,11 @@ let game = {
 
     input_to_coordinates(input_field) {
         let grid_1 = [
-            ["1","2","3"],
-            ["4","5","6"],
-            ["7","8","9"]
+            ["1","2","3","4","5"],
+            ["6","7","8","9","10"],
+            ["11","12","13","14","15"],
+            ["16","17","18","19","20"],
+            ["21","22","23","24","25"]
         ];
 
         for(let i = 0; i < grid_1.length; i++) {
@@ -56,19 +59,15 @@ let game = {
 
   find_neighbors(grid, row, column, neighbors) {
 
-
     if (row > 0 && grid[row - 1][column]) {
         neighbors.push(grid[row - 1][column]);
     } 
-  
     if (row < grid.length - 1 && grid[row + 1][column]) {
         neighbors.push(grid[row + 1][column]);
-    } 
-      
+    }  
     if (grid[row][column - 1]) {
         neighbors.push(grid[row][column - 1]);
     } 
-   
     if (grid[row][column + 1]) {
         neighbors.push(grid[row][column + 1]);
     }
@@ -84,10 +83,10 @@ let game = {
                 for(let j = 0; j < grid[i].length; j++){
                   field++;
                   if(field === e) {
-                   if(grid_game[i][j] == "1"){
-                    grid_game[i][j] = "0";
-                   } else {
-                    grid_game[i][j] = "1";
+                    if(grid_game[i][j] == "1"){
+                        grid_game[i][j] = "0";
+                    } else {
+                        grid_game[i][j] = "1";
                    };
                   }
                 }
@@ -101,27 +100,24 @@ let game = {
         } else {
             grid_game[this.selection[0]][this.selection[1]] = "1";
         }
-        
         this.check_for_winner();
     },
 
     check_for_winner(){
-
         let row_found = true;
         for(let i = 0; i < grid_game.length; i++) {
-           
             for(let j = 0; j < grid_game.length; j++){
-              row_found = row_found && grid_game[i][j] == "0";
+                row_found = row_found && grid_game[i][j] == "0";
             }
         }
 
         if(row_found == true){
-    
-            console.log(grid_game)
             alert(`
             ${grid_game[0]} 
             ${grid_game[1]}
             ${grid_game[2]}
+            ${grid_game[3]}
+            ${grid_game[4]}
         `);
             alert("game won");
             game_over = true;
@@ -130,9 +126,9 @@ let game = {
             ${grid_game[0]} 
             ${grid_game[1]}
             ${grid_game[2]}
+            ${grid_game[3]}
+            ${grid_game[4]}
         `);
-
-            console.log(grid_game)
             game.start();
         }
     },
